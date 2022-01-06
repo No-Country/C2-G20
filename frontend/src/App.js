46f284f18b5e4cef827d0a6896ba36c22cef1641
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-05 20:10:17 -0500

    arreglado grid de catalogo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-import "./App.css"
-import Header from "./components/Header"
-=======
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+
+import logo from "./logo.svg";
+import "./App.css";
 import Retail from "./components/Retail";
 import Footer from "./components/Footer";
->>>>>>> f1381db51d6f50ae9e64809af72b50762bcec269
 
 function App() {
   return (
@@ -16,7 +13,6 @@ function App() {
       <section className="section-retail">
         <Retail />
       </section>
-      <Retail />
     </div>
   );
 }
--- frontend/src/components/Retail/index.js
+++ frontend/src/components/Retail/index.js
@@ -1,9 +1,31 @@
-import { Icon } from "@iconify/react"
 
 export default function Header() {
   return (
     <div className="retail-box-container">
-
+      <div className="retail-box">
+        <div className="retail-box-image"></div>
+        <button className="retail-box-button">AGREGAR</button>
+      </div>
+      <div className="retail-box">
+        <div className="retail-box-image"></div>
+        <button className="retail-box-button">AGREGAR</button>
+      </div>
+      <div className="retail-box">
+        <div className="retail-box-image"></div>
+        <button className="retail-box-button">AGREGAR</button>
+      </div>
+      <div className="retail-box">
+        <div className="retail-box-image"></div>
+        <button className="retail-box-button">AGREGAR</button>
+      </div>
+      <div className="retail-box">
+        <div className="retail-box-image"></div>
+        <button className="retail-box-button">AGREGAR</button>
+      </div>
+      <div className="retail-box">
+        <div className="retail-box-image"></div>
+        <button className="retail-box-button">AGREGAR</button>
+      </div>
     </div>
-  )
+  );
 }
