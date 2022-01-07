9d47ef296cffc4ec5fac73bba5b170c1cbf3126b
Marcio <marciofcb@gmail.com>
2022-01-06 22:56:30 -0300

    componente Footer refactoreado y efecto acordeon mejorado

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,27 +1,26 @@
 import { Icon } from "@iconify/react"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import './Footer.css'
 
 function Links ({link}) {
   return <a href="/#"><p style={{padding: "10px 5%"}}>{link}</p></a>
 }
 
-function Details ({detail, links}) {
-  const [clicked, setClicked] = useState(false)
-  useEffect(() => {}, [clicked])
-
+function Details ({detail, open, setOpen, children}) {
   return <>
     <div className="detail">
-      <div className="detail-title" onClick={() => setClicked(!clicked)}>
+      <div className="detail-title" onClick={() => setOpen(detail)}>
         <span>{detail}</span>
-        <Icon icon={`bx:bxs-${clicked ? 'up' : 'down'}-arrow`} color="#017adb" width="20" height="20" />
+        <Icon icon={`bx:bxs-${detail === open ? 'up' : 'down'}-arrow`} color="#017adb" width="20" height="20" />
       </div>
-      {clicked && links}
+      {detail === open && children}
     </div>
   </>
 }
 
 export default function Footer () {
+  const [open, setOpen] = useState(null)
+
   return (
     <footer>
       <div className="contactos">
@@ -31,20 +30,22 @@ export default function Footer () {
         <a href="https://youtube.com" target="_blank" rel="noreferrer noopener"><Icon icon="simple-icons:youtube" color="white" width="20" height="20"/></a>
       </div>
       <div className="footer-links">
-        <Details detail={'Grupo.com.mx'} 
-          links={<>
+        <Details detail={'Grupo.com.mx'} open={open} setOpen={setOpen}>
           <Links link={"Quienes Somos"} />
-          <Links link={"Aviso de Privacidad"} /></>} />
-        <Details detail={'Servicio a Domicilio'}
-          links={<Links link={"Correo"}/>} />
-        <Details detail={'Ayuda'} 
-          links={<Links link={"Soporte"}/>} />
-        <Details detail={'Info Corporativa'} 
-          links={<>
-            <Links link={"Direccion 1"} />
-            <Links link={"Direccion 2"} />
-            <Links link={"Direccion 3"} />
-            <Links link={"Direccion 4"} /></>} />
+          <Links link={"Aviso de Privacidad"} />
+        </Details>
+        <Details detail={'Servicio a Domicilio'} open={open} setOpen={setOpen}>
+          <Links link={"Correo"}/>
+        </Details>
+        <Details detail={'Ayuda'} open={open} setOpen={setOpen}>
+          <Links link={"Soporte"}/>
+        </Details>
+        <Details detail={'Info Corporativa'} open={open} setOpen={setOpen}>
+          <Links link={"Direccion 1"} />
+          <Links link={"Direccion 2"} />
+          <Links link={"Direccion 3"} />
+          <Links link={"Direccion 4"} />
+        </Details>
       </div>
       <div className="copyright">
         <p>Grupo20 Mx y Centroamérica</p>
