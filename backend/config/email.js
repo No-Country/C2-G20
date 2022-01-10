51da915bdc56069016e58fc36acce0915eca313c
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-10 14:46:54 -0500

    clean

--- backend/config/email.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  user: "2482ebfe31c4d5",
-  pass: "76bce2c90db6c0",
-  host: "smtp.mailtrap.io",
-  port: 2525,
-};
--- backend/public/js/app.js
+++
@@ -1,7 +0,0 @@
-import projects from "./modules/projects";
-import tasks from "./modules/tasks";
-import { updateAdvance } from "./functions/advance";
-
-document.addEventListener("DOMContentLoaded", () => {
-  updateAdvance();
-});
