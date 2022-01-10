bf351f5d62b0054676dd172eddd91a9ff1feff92
Sebastian Mosquera <sebimosquera@gmail.com>
2022-01-10 14:42:05 -0300

    se elimino la seccion retail

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,10 @@
-import Retail from "../../components/Retail"
+
+
 
 export default function Home() {
   return (
-    <section className="section-retail">
-      <Retail />
+    <section className="">
+     
     </section>
   )
 }
