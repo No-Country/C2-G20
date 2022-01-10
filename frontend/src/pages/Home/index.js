cc2b9b85d1c60f01991a34c8a702d323ce763209
Sebastian Mosquera <sebimosquera@gmail.com>
2022-01-10 14:42:48 -0300

    se realizo componente login

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,10 @@
 
-
+import { Login }  from "../../components/Login/Login";
 
 export default function Home() {
   return (
     <section className="">
-     
+     <Login />
     </section>
   )
 }
