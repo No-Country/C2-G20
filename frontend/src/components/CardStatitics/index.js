09214ec01956cd21fdfd731b1de55b57c64a21d0
Sebastian Mosquera <sebimosquera@gmail.com>
2022-01-11 15:12:42 -0300

    se arreglo responsive del nav dashboard

--- frontend/src/components/CardStatitics/index.js
+++ frontend/src/components/CardStatitics/index.js
@@ -2,7 +2,7 @@ import "./index.css"
 
 export default function CardStatitics({ title, image }) {
   return (
-    <figure className="card-statitics">
+    <figure className="card-statitics my-2 w-100">
       <h2>{title}</h2>
       <img src={image} alt={title} />
     </figure>
--- frontend/src/components/ElipseStatitics/index.js
+++ frontend/src/components/ElipseStatitics/index.js
@@ -2,7 +2,7 @@ import "./index.css"
 
 export default function ElipseStatitics({ number, description }) {
   return (
-    <figure className="container-elipse">
+    <figure className="container-elipse w-75 container">
       <div className="elipse">
         <h2 clasName="number-elipse">{number}</h2>
       </div>
