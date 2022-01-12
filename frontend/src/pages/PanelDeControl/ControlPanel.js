8507a246a02b91c72cec2d5b8463e6f86478e3dd
Sebastian Mosquera <sebimosquera@gmail.com>
2022-01-12 18:47:31 -0300

    se realiza la maquetacion del panel del control

--- frontend/src/pages/PanelDeControl/ControlPanel.js
+++ frontend/src/pages/PanelDeControl/ControlPanel.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import Footer from '../../components/Footer'
-import logo from '../../images/icon-for-dashboard-15.jpg'
+import React from "react";
+import Footer from "../../components/Footer";
+import logo from "../../images/dashboardgif.gif";
+import "animate.css";
 export const ControlPanel = () => {
-    return (
-        <div className="border container shadow-lg my-4 pb-5">
-            <h1 className="my-4 lead display-4">Panel de control</h1>
-            <hr className="bg-primary" />
-            <img src={logo} className="w-25" />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="border container shadow-lg my-4 mr-4 pb-5">
+      <h1 className="my-2  text-primary">Panel de control</h1>
+      <hr className="bg-primary" />
+      <h4  className="animate__animated animate__fadeInDown">¡Hola Usuario!</h4>
+      <p className="form-text  animate__animated animate__fadeInDown">
+        Bienvenido al dashboard de <b>cryptocurrency.</b>{" "}
+      </p>
+      <p className="lead  animate__animated animate__fadeInDown">
+        Utiliza el menu de navegación que se encuentra en el lado izquierdo para
+        poder previsualizar las estadisticas.
+      </p>
+      <img
+        src={logo}
+        className="w-50  animate__animated animate__fadeIn"
+        alt="logo dashboard "
+      />
+      <Footer />
+    </div>
+  );
+};
