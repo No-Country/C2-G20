5a133452155da120f67a702dedaaa47e37e0b016
Marcio <marciofcb@gmail.com>
2022-01-12 21:25:45 -0300

    Chart.js integrado y Dashboard creado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Retail from "./components/Retail";
 import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard/Dashboard"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section className="section-retail">
-        <Retail />
-      </section>
-      <Retail />
+      <Dashboard />
       <Footer />
     </div>
   );
