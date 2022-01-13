4d5ca0a07baa92fca00aacc305ed4e1427275ae4
Sebastian Mosquera <sebimosquera@gmail.com>
2022-01-13 19:23:20 -0300

    se implemento momentjs para el uso de fechas

--- frontend/src/pages/PanelDeControl/ControlPanel.js
+++ frontend/src/pages/PanelDeControl/ControlPanel.js
@@ -2,14 +2,20 @@ import React from "react";
 import Footer from "../../components/Footer";
 import logo from "../../images/dashboardgif.gif";
 import "animate.css";
+import moment from 'moment'
+import 'moment/locale/es'
 export const ControlPanel = () => {
   return (
+
+
     <div className="border container shadow-lg my-4 mr-4 pb-5">
-      <h1 className="my-2  text-primary">Panel de control</h1>
+      <div>
+        <p className="form-text">{moment().locale('es').format('LLLL')}</p>
+      </div>
       <hr className="bg-primary" />
       <h4  className="animate__animated animate__fadeInDown">¡Hola Usuario!</h4>
-      <p className="form-text  animate__animated animate__fadeInDown">
-        Bienvenido al dashboard de <b>cryptocurrency.</b>{" "}
+      <p className="form-text  animate__animated animate__fadeInDown  ">
+        Bienvenido al dashboard de <b className="text-primary">cryptocurrency.</b>{" "}
       </p>
       <p className="lead  animate__animated animate__fadeInDown">
         Utiliza el menu de navegación que se encuentra en el lado izquierdo para
