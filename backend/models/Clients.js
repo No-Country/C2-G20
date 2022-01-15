d053f63cd80400c0fc9f621bc06a861b984ed9db
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-15 18:41:17 -0500

    Bcrypt

--- backend/models/Clients.js
+++ backend/models/Clients.js
@@ -1,23 +1,39 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
-const Clients = db.define("clients", {
-  symbol_crypto: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Clients = db.define(
+  "clients",
+  {
+    symbol_crypto: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING(70),
+    },
+    email: {
+      type: Sequelize.STRING(70),
+    },
+    password: {
+      type: Sequelize.STRING(70),
+    },
+    verified: {
+      type: Sequelize.BOOLEAN,
+    },
   },
-  name: {
-    type: Sequelize.STRING(70),
-  },
-  email: {
-    type: Sequelize.STRING(70),
-  },
-  password: {
-    type: Sequelize.STRING(70),
-  },
-  verified: {
-    type: Sequelize.BOOLEAN,
-  },
-});
+  {
+    hooks: {
+      beforeCreate: (client) => {
+        client.password = bcrypt.hashSync(client.password, 10);
+      },
+    },
+  }
+);
+
+Clients.prototype.verifyPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 module.exports = Clients;
