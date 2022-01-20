19f443d2cb7f685348515ad5de6948f5c5b2979b
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-20 13:04:25 -0500

    change port

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(
 
 // Server and port
 const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
