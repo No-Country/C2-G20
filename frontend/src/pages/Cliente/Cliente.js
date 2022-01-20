d22004327e7304d8b056f28c53fda442827aa4c6
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-20 13:20:09 -0500

    again

--- frontend/src/pages/Cliente/Cliente.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Cliente = () => {
-    return (
-        <div>
-            <h1>Cliente ??</h1>
-        </div>
-    )
-}
--- frontend/src/pages/Configuracion/Configuracion.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Configuracion = () => {
-    return (
-        <div>
-            <h1>Configuracion de usuario</h1>
-        </div>
-    )
-}
