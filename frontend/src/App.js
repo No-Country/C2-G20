ba2ecb283220ffadc79ba21b41655633c3b1b737
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-21 00:38:16 -0500

    context first impresion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
+import { useContext } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import "./App.css";
-import Footer from "./components/Footer";
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,29 +8,37 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import DashboardStatitics from "./pages/DashboardStatitics";
-import HeaderDashboard from "./components/HeaderDashboard";
 import { ControlPanel } from "./pages/PanelDeControl/ControlPanel";
 import { Configuracion } from "./pages/Configuracion/Configuracion";
 import { Cliente } from "./pages/Cliente/Cliente";
 import NavBar from "./components/Nav";
 
-import { CryptoContext } from "./context/CryptoContext";
+import { CryptoContext, CryptoProvider } from "./context/CryptoContext";
 
 function App() {
+  const { auth, setAuth } = useContext(CryptoContext);
   const { pathname } = useLocation();
   return (
     <div className="App">
-      {!pathname.includes("/Dashboard") && !pathname.includes("/dashboard") ? (
-        <Home />
-      ) : (
-        <NavBar />
-      )}
-      <Routes>
-        <Route path="/Dashboard" element={<ControlPanel />} />
-        <Route path="/Dashboard/Statitics" element={<DashboardStatitics />} />
-        <Route path="/Dashboard/Setting" element={<Configuracion />} />
-        <Route path="/Dashboard/Cliente" element={<Cliente />} />
-      </Routes>
+      <>
+        <CryptoProvider value={[auth, setAuth]}>
+          {!pathname.includes("/Dashboard") &&
+          !pathname.includes("/dashboard") ? (
+            <Home />
+          ) : (
+            <NavBar />
+          )}
+          <Routes>
+            <Route path="/Dashboard" element={<ControlPanel />} />
+            <Route
+              path="/Dashboard/Statitics"
+              element={<DashboardStatitics />}
+            />
+            <Route path="/Dashboard/Setting" element={<Configuracion />} />
+            <Route path="/Dashboard/Cliente" element={<Cliente />} />
+          </Routes>
+        </CryptoProvider>
+      </>
     </div>
   );
 }
--- frontend/src/context/CryptoContext.js
+++ frontend/src/context/CryptoContext.js
@@ -3,13 +3,13 @@ import { useState, createContext } from "react";
 const CryptoContext = createContext([{}, () => {}]);
 
 const CryptoProvider = (props) => {
-  const [auth, saveToken] = useState({
+  const [auth, setAuth] = useState({
     token: "",
     auth: false,
   });
 
   return (
-    <CryptoContext.Provider value={[auth, saveToken]}>
+    <CryptoContext.Provider value={[auth, setAuth]}>
       {props.children}
     </CryptoContext.Provider>
   );
