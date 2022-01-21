47bd322ce9082e6e5788d75184e44b52804ffbc0
Marcio <marciofcb@gmail.com>
2022-01-21 05:11:57 -0300

    Nav componente refactoreado

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,8 +1,21 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
+const NavItem = ({item, link, icon, active, ...rootDOMAttributes}) => {
+  return <li className="nav-item" {...rootDOMAttributes}>
+    <Link to={link} className={`nav-link ${active === item ? 'nav-link-active' : 'nav-link-inactive'}`}>
+      <Icon icon={icon} width="32" height="32" />
+      <span className="link-text">{item}</span>
+    </Link>
+  </li>
+}
+
 export default function NavBar () {
+  const [active, setActive] = useState(false)
+  const [theme, setTheme] = useState(false)
+
   return <nav className="navbar">
       <ul className="navbar-nav">
         <li className="logo">
@@ -16,53 +29,53 @@ export default function NavBar () {
               role="img"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
-              class="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
+              className="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
             >
-              <g class="fa-group">
+              <g className="fa-group">
                 <path
                   fill="currentColor"
                   d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
-                  class="fa-secondary"
+                  className="fa-secondary"
                 ></path>
                 <path
                   fill="currentColor"
                   d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
-                  class="fa-primary"
+                  className="fa-primary"
                 ></path>
               </g>
             </svg>
           </Link>
         </li>
-        <li className="nav-item">
-          <Link to='/Dashboard' className="nav-link ">
-            <Icon icon="fluent:home-16-filled" width="32" height="32"/>
-            <span className="link-text">Dashboard</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to='/Dashboard/Statitics' className="nav-link">
-            <Icon icon="whh:statistics" width="32" height="32"/>
-            <span className="link-text">Statitics</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to='/Dashboard/Setting' className="nav-link">
-            <Icon icon="zmdi:settings" width="32" height="32" />
-            <span className="link-text">Setting</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to='/Dashboard/Cliente' className="nav-link">
-            <Icon icon="ph:users-bold" width="32" height="32"/>
-            <span className="link-text">Cliente</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="#" className="nav-link">
-            <Icon icon="eva:moon-fill" width="32" height="32"/>
-            <span className="link-text">Soon...</span>
-          </Link>
-        </li>
+        <NavItem 
+          item={'Dashboard'}
+          link={'/Dashboard'}
+          icon={"fluent:home-16-filled"}
+          active={active}
+          onClick={()=>setActive('Dashboard')}/>
+        <NavItem 
+          item={'Statitics'}
+          link={'/Dashboard/Statitics'}
+          icon={"whh:statistics"}
+          active={active}
+          onClick={()=>setActive('Statitics')}/>
+        <NavItem 
+          item={'Setting'}
+          link={'/Dashboard/Setting'}
+          icon={'fluent:settings-28-filled'}
+          active={active}
+          onClick={()=>setActive('Setting')}/>
+        <NavItem 
+          item={'Cliente'}
+          link={'/Dashboard/Cliente'}
+          icon={"ph:users-bold"}
+          active={active}
+          onClick={()=>setActive('Cliente')}/>
+        <NavItem 
+          item={'Soon...'}
+          link={'#'}
+          icon={`${ theme ? 'ic:round-wb-sunny' : "eva:moon-fill"}`}
+          active={active}
+          onClick={()=>setTheme(!theme)}/>
       </ul>
     </nav>
 }
