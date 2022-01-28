06abf50380dcc69eeaca3477a648552476cf1cc7
Marcio <marciofcb@gmail.com>
2022-01-28 13:43:46 -0300

    cambios de UI en DashboardStatics

--- frontend/src/pages/DashboardStatitics/index.js
+++ frontend/src/pages/DashboardStatitics/index.js
@@ -1,10 +1,8 @@
-// import "./index.css"
 import Dashboard from "../../components/Dashboard/Dashboard"
-import BartChart from "../../components/Dashboard/BarChart"
 import React, { useState, useEffect, useCallback } from "react"
 import PieChart from "../../components/Dashboard/PieChart"
 import LineChart from "../../components/Dashboard/LineChart"
-import "../DashboardStatitics/dashboardstatitics.css"
+import "./dashboardstatitics.css"
 import DoughnutChart from "../../components/Dashboard/DoughnutChart"
 import axios from "axios"
 import useValues from "../../hooks/useValues"
@@ -13,26 +11,34 @@ export default function DashboardStatitics() {
   const [pieChart, setPieChart] = useState(false)
   const [dashboard, setDashboard] = useState(true)
   const [lineChart, setLineChart] = useState(false)
-  const { valuesOtherDay, valuesToday } = useValues()
+  const { valuesOtherDay, valuesToday } = useValues("btc")
 
-  const lineChartEnabled = () => {
-    setLineChart(true)
-    setDashboard(false)
-  }
-
-  const lineChartDisabled = () => {
+  const showBar = () => {
+    setDashboard(true)
     setLineChart(false)
-    setDashboard(true)
-  }
-
-  const pieChartEnabled = () => {
-    setPieChart(true)
-    setDashboard(false)
-  }
-
-  const pieChartDisabled = () => {
     setPieChart(false)
-    setDashboard(true)
+  }
+  const showLine = () => {
+    if(lineChart) {
+      setLineChart(false)
+      setDashboard(true)
+      setPieChart(false)
+    } else {
+      setLineChart(true)
+      setDashboard(false)
+      setPieChart(false)
+    }
+  }
+  const showPie = () => {
+    if(pieChart) {
+      setPieChart(false)
+      setDashboard(true)
+      setLineChart(false)
+    } else {
+      setPieChart(true)
+      setDashboard(false)
+      setLineChart(false)
+    }
   }
 
   const payload = useCallback(
@@ -62,75 +68,64 @@ export default function DashboardStatitics() {
   return (
     <section
       className="dashboard border shadow-lg my-4"
-      style={{ "padding-left": "1em" }}
+      style={{ paddingLeft: "1em" }}
     >
-      <h4 className="lead display-6">BTC Hoy - 2022-01-24</h4>
+      <h4 className="lead display-6">BTC Ultimo mes</h4>
       <hr className="text-primary mb-4" />
       <div className="row">
         <div className="col-md-9 col-sm-12">
           {dashboard ? <Dashboard /> : null}
           {pieChart ? <PieChart width={500} height={500} /> : null}
-
           {lineChart ? <LineChart width={500} height={300} /> : null}
         </div>
 
         <div className="col-md-3 col-sm-12 mb-4">
           <div className="my-3">
-            {!lineChart ? (
-              <button
-                className="btn btn-outline-primary"
-                onClick={lineChartEnabled}
-              >
-                <i class="fas fa-chart-line"></i>
-              </button>
-            ) : (
-              <button className="btn btn-danger" onClick={lineChartDisabled}>
-                <i class="fas fa-chart-line"></i>
-              </button>
-            )}
+            <button className={!dashboard ? "btn btn-outline-primary" : "btn btn-primary"} onClick={showBar}>
+              <i className="fas fa-chart-bar"></i>
+            </button>
+            <p className="form-text lead">Bar Chart</p>
           </div>
           <hr />
-          {!pieChart ? (
-            <button
-              className="btn btn-outline-primary"
-              onClick={pieChartEnabled}
-            >
-              <i class="fas fa-chart-pie"></i>
+          <div className="my-3">
+            <button className={!lineChart ? "btn btn-outline-primary" : "btn btn-primary"} onClick={showLine}>
+              <i className="fas fa-chart-line"></i>
             </button>
-          ) : (
-            <button className="btn btn-danger" onClick={pieChartDisabled}>
-              <i class="fas fa-chart-pie"></i>
+            <p className="form-text lead">Line Chart</p>
+          </div>
+          <hr />
+          <div className="my-3">
+		        <button className={!pieChart ? "btn btn-outline-primary" : "btn btn-primary"} onClick={showPie}>
+              <i className="fas fa-chart-pie"></i>
             </button>
-          )}
+            <p className="form-text lead">Pie Chart</p>
+          </div>
           <hr />
           <div className="my-3">
             <button className="btn btn-outline-success" onClick={downloadExcel}>
-              <i class="fas fa-file-excel"></i>
+              <i className="fas fa-file-excel"></i>
             </button>
-            <p className="form-text lead">Exportar a excel</p>
+            <p className="form-text lead">Exportar Excel</p>
           </div>
         </div>
       </div>
       <h4 className="lead display-6 my-3">BTC Hoy</h4>
       <hr className="text-primary" />
 
-      <div className="container mb-4 mx-5 row">
+      <div className="container mb-4 row">
         <div className="col-md-9">
           <DoughnutChart width={500} height={400} />
         </div>
 
         <div className="col-md-3">
           <p className="lead mx-4">
-            USD: <i class="fas fa-dollar-sign text-primary"></i>{" "}
-            {valuesToday ? valuesToday.usd : 0}
+            ARS: <i className="fas fa-dollar-sign text-primary"></i> 4,414,892
           </p>
           <p className="lead mx-4">
-            MXN: <i class="fas fa-dollar-sign text-primary"></i>{" "}
-            {valuesToday ? valuesToday.mxn : 0}
+            MXN: <i className="fas fa-dollar-sign text-primary"></i> 864,902
           </p>
           <p className="lead mx-4">
-            EUR: <i class="fas fa-euro-sign text-primary"></i>{" "}
-            {valuesToday ? valuesToday.eur : 0}
+            EUR: <i className="fas fa-euro-sign text-primary"></i> 37,253
           </p>
         </div>
       </div>
